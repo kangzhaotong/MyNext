3b731f46338eccaaf3f393b17ecebae2a4f1eb57
yx-0070 <kangzt@flamelephant.com>
2023-02-17 15:30:03 +0800

    初始化

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+import MyTodo from './myTodo/index'
+const  HomePage = () => {
+
+    return <div>Welcome to Next.js!<MyTodo>asdasdadadad</MyTodo></div>
+}
+  
+  export default HomePage
